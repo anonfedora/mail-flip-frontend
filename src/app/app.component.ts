005051ad917906947f98ac5e571aff295b4f9887
anonfedora <musaeleazar090@gmail.com>
2024-04-19 12:50:50 +0100

    form components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,4 +31,58 @@ export class AppComponent {
             message: ["", Validators.required]
         });
     }
+
+    onSubmit() {
+        if (this.contactForm.valid) {
+            const formValues = this.contactForm.value;
+            console.log("Form submitted with values:", formValues);
+
+            const formData = new FormData();
+            formData.append("name", formValues.name);
+            formData.append("subject", formValues.subject);
+            formData.append("email", formValues.email);
+            formData.append("message", formValues.message);
+
+            this.emailService.sendEmail(formData).subscribe({
+                next: (response: HttpResponse<any>) => {
+                    if (response.ok) {
+                        this.successMessage = response.body.message;
+                        this.showForm = false;
+                        this.clearSuccessMessage();
+                        this.selectedFile = null;
+                        this.contactForm.reset();
+                    }
+                },
+                error: (error: any) => {
+                    console.error("Error sending email:", error);
+                }
+            });
+        } else {
+            // Form is invalid, display error messages
+            // Additional validation logic or user feedback here
+        }
+    }
+
+    clearSuccessMessage() {
+        setTimeout(() => {
+            this.successMessage = "";
+            this.showForm = true;
+        }, 5000);
+    }
+
+    get name() {
+        return this.contactForm.get("name");
+    }
+
+    get subject() {
+        return this.contactForm.get("subject");
+    }
+
+    get email() {
+        return this.contactForm.get("email");
+    }
+
+    get message() {
+        return this.contactForm.get("message");
+    }
 }
